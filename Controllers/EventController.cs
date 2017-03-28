0ba0d29359b2d7353e7e59fe51f9ba758e81e10e
Michael Sadahiro <michaelsadahiro@gmail.com>
2017-03-28 12:01:49 -0700

    add show one page

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -26,8 +26,9 @@ namespace events.Controllers
             int? getUserId = HttpContext.Session.GetInt32("CurrentUser");
             User SignedInUser = _context.Users.Where(User => User.id == getUserId).SingleOrDefault();
             List<Event> getAllEvents = _context.Events
-            .Include(reserve => reserve.Attendings)
-            .ToList();
+                .Include(reserve => reserve.Attendings)
+                    .ThenInclude(Reserve => Reserve.User)
+                .ToList();
             ViewBag.CurrentUser = SignedInUser;
             ViewBag.AllEvents = getAllEvents;
             return View();
@@ -47,9 +48,16 @@ namespace events.Controllers
             if(HttpContext.Session.GetInt32("CurrentUser")==null){
                 return RedirectToAction("Login","User");
             }
+            int? getUserId = HttpContext.Session.GetInt32("CurrentUser");
             if(ModelState.IsValid){
                 newEvent.CreatedBy = (int)HttpContext.Session.GetInt32("CurrentUser");
                 _context.Add(newEvent);
+                _context.SaveChanges();
+                Reserve newRsvp = new Reserve(){
+                    UserId = (int)getUserId,
+                    EventId = newEvent.id
+                };
+                _context.Add(newRsvp);
                 _context.SaveChanges();
                 return RedirectToAction("Dashboard");
             }
@@ -57,6 +65,30 @@ namespace events.Controllers
                 ViewBag.errors = ModelState.Values;
                 return View("New");
             }
+        }
+        [HttpGet]
+        [RouteAttribute("activity/{id}")]
+        public IActionResult Activity(int id){
+            if(HttpContext.Session.GetInt32("CurrentUser")==null){
+                return RedirectToAction("Login","User");
+            }
+            List<Event> showActivity = _context.Events.Where(activity => activity.id == id)
+                .Include(attending => attending.Attendings)
+                .ToList();
+            ViewBag.Activity = showActivity;
+            return View("Activity");
+        }
+        [HttpGet]
+        [RouteAttribute("reserve/{id}")]
+        public IActionResult Reserve(int id){
+            int? getUserId = HttpContext.Session.GetInt32("CurrentUser");
+            Reserve newRsvp = new Reserve(){
+                UserId = (int)getUserId,
+                EventId = id
+            };
+            _context.Add(newRsvp);
+            _context.SaveChanges();
+            return RedirectToAction("Dashboard");
         }
     }
 }
